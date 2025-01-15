50fe540b4c81145621a82d23a76e6863f8c26519
Robin Samuelkutty <robinsam754@gmail.com>
2025-01-15 21:52:26 +0530

    clerk completed and started home section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import type { Metadata } from "next";
 import 'tailwindcss/tailwind.css'
-
+import {ClerkProvider} from "@clerk/nextjs"
 import { Inter } from "next/font/google";
 
 import "@stream-io/video-react-sdk/dist/css/styles.css";
@@ -24,12 +24,26 @@ export default function RootLayout({
 }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
-      
+      <ClerkProvider 
+      appearance={{
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/icons/logo.svg",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1C1F2E",
+            colorInputBackground: "#252A41",
+            colorInputText: "#fff",
+          },
+        }}>
         <body className={`${inter.className} bg-dark-2`}>
           
           {children}
+          
         </body>
-     
+      </ClerkProvider>
     </html>
   );
 }
